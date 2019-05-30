9441b87c55e4f8f0ce3a53914b5e4b0fa80c8255
Aymerick <aymerick@jehanne.org>
2019-05-30 18:55:15 +0200

    feat(domoticz): send motion sensor data to domoticz

--- pkg/domoticz/domoticz.go
+++ pkg/domoticz/domoticz.go
@@ -44,13 +44,13 @@ func (h *Handler) Push(packet *homlet.Packet, settings *homlet.DeviceSettings) e
 }
 
 func (h *Handler) pushCommand(packet *homlet.Packet, sensor homlet.Sensor, settings *homlet.SensorSettings) error {
-	p := h.params(packet, sensor)
+	p := h.params(packet, sensor, settings.Domoticz)
 	if p == "" {
 		return nil
 	}
 
 	// FIXME ?? send vcc as battery level: https://www.domoticz.com/wiki/Domoticz_API/JSON_URL's#Additional_parameters_.28signal_level_.26_battery_level.29
-	url := fmt.Sprintf("%s/json.htm?type=command&param=udevice&idx=%d&%s", h.URL, settings.Domoticz, p)
+	url := fmt.Sprintf("%s/json.htm?type=command&%s", h.URL, p)
 
 	log.Debugf("Pushing to domoticz: %s", url)
 
@@ -72,30 +72,31 @@ func (h *Handler) pushCommand(packet *homlet.Packet, sensor homlet.Sensor, setti
 	return nil
 }
 
-func (h *Handler) params(packet *homlet.Packet, sensor homlet.Sensor) string {
+func (h *Handler) params(packet *homlet.Packet, sensor homlet.Sensor, deviceID int) string {
 	switch sensor {
 	case homlet.Temperature:
 		if packet.HaveSensor(homlet.Humidity) {
 			// https://www.domoticz.com/wiki/Domoticz_API/JSON_URL's#Temperature.2Fhumidity
-			return fmt.Sprintf("nvalue=0&svalue=%.1f;%d;%d", packet.Temperature, packet.Humidity, humidityStatus(packet.HumidityStatus()))
+			return fmt.Sprintf("param=udevice&idx=%d&nvalue=0&svalue=%.1f;%d;%d", deviceID, packet.Temperature, packet.Humidity, humidityStatus(packet.HumidityStatus()))
 		}
 		// https://www.domoticz.com/wiki/Domoticz_API/JSON_URL's#Temperature
-		return fmt.Sprintf("nvalue=0&svalue=%.1f", packet.Temperature)
+		return fmt.Sprintf("param=udevice&idx=%d&nvalue=0&svalue=%.1f", deviceID, packet.Temperature)
 
 	case homlet.Humidity:
 		// https://www.domoticz.com/wiki/Domoticz_API/JSON_URL's#Humidity
-		return fmt.Sprintf("nvalue=%d&svalue=%d", packet.Humidity, humidityStatus(packet.HumidityStatus()))
+		return fmt.Sprintf("param=udevice&idx=%d&nvalue=%d&svalue=%d", deviceID, packet.Humidity, humidityStatus(packet.HumidityStatus()))
 
 	case homlet.Light:
 		// https://www.domoticz.com/wiki/Domoticz_API/JSON_URL's#Percentage
-		return fmt.Sprintf("nvalue=0&svalue=%d", packet.Light)
+		return fmt.Sprintf("param=udevice&idx=%d&nvalue=0&svalue=%d", deviceID, packet.Light)
 
 	case homlet.Motion:
-		// FIXME send to 'text', 'custom' or 'switch' virtual device ?
-		//   https://www.domoticz.com/wiki/Domoticz_API/JSON_URL's#Text_sensor
-		//   https://www.domoticz.com/wiki/Domoticz_API/JSON_URL's#Toggle_a_switch_state_between_on.2Foff
-		//   https://www.domoticz.com/wiki/Domoticz_API/JSON_URL's#Custom_Sensor
-		return ""
+		// https://www.domoticz.com/wiki/Domoticz_API/JSON_URL's#Turn_a_light.2Fswitch_on
+		cmd := "Off"
+		if packet.Motion {
+			cmd = "On"
+		}
+		return fmt.Sprintf("param=switchlight&idx=%d&switchcmd=%s", deviceID, cmd)
 	}
 
 	// unsupported sensor
